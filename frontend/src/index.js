86cfa72f67c05d0d59e8b9be651209a07b54b528
julien <git@julienc.io>
2019-02-03 23:32:01 +0100

    updated frontend dependencies

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,9 +10,12 @@ import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import createHistory from 'history/createBrowserHistory'
+import { unregister } from './registerServiceWorker'
 
 import App from './components/App'
 import Tracker from './components/Tracker'
+
+unregister()
 
 const httpLink = new HttpLink({ uri: `${process.env.REACT_APP_REDIRECT_DOMAIN}/graphql` })
 
