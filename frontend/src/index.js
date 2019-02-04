0127834a2821c3958a40d1c007151743969e38f5
julien <git@julienc.io>
2019-02-04 19:54:53 +0100

    yarn building issue

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,12 +10,10 @@ import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import createHistory from 'history/createBrowserHistory'
-import { unregister } from './registerServiceWorker'
+import * as serviceWorker from './serviceWorker'
 
 import App from './components/App'
 import Tracker from './components/Tracker'
-
-unregister()
 
 const httpLink = new HttpLink({ uri: `${process.env.REACT_APP_REDIRECT_DOMAIN}/graphql` })
 
@@ -37,3 +35,5 @@ ReactDOM.render(
   </IntlProvider>,
   document.getElementById('root')
 )
+
+serviceWorker.unregister();
