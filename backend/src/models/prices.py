9faacf1397a7404ebb6a4e90ab01e4109a950931
julien <git@julienc.io>
2019-08-02 21:25:21 +0200

    breaking change in dependency makes unique on subdocument not behaving as expected

--- backend/src/models/prices.py
+++ backend/src/models/prices.py
@@ -9,7 +9,7 @@ from .enums import Providers
 
 class Price(ModelMixin, db.EmbeddedDocument):
 
-    _provider = db.StringField(db_field="provider", choices=Providers, required=True, unique=True)
+    _provider = db.StringField(db_field="provider", choices=Providers, required=True)
     price = db.FloatField(required=True)
     creation_date = db.DateTimeField(required=True, default=datetime.now)
     update_date = db.DateTimeField(requured=True, default=datetime.now)
