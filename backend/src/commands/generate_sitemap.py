5f520856d03af582813b2d1dacb152d14bfd0940
julien <git@julienc.io>
2020-09-26 18:29:50 +0200

    updating sitemap generator

--- backend/src/commands/generate_sitemap.py
+++ backend/src/commands/generate_sitemap.py
@@ -9,24 +9,27 @@ from ..models.csgo import Skin
 
 class GenerateSitemap:
 
-    base_urls = [
-        ["https://lionskins.co/", "monthly", 0.9],
-        ["https://lionskins.co/about/", "never", 0.3],
-        ["https://lionskins.co/contact/", "never", 0.3],
-        ["https://lionskins.co/faq/", "monthly", 0.3],
-        ["https://lionskins.co/privacy-policy/", "never", 0.1],
-        ["https://lionskins.co/counter-strike-global-offensive/", "daily", 1],
+    base_url = "https://lionskins.co"
+    languages = ["en", "fr"]
+    static_pages = [
+        ["", "monthly", 0.9],
+        ["about", "never", 0.3],
+        ["contact", "never", 0.3],
+        ["faq", "monthly", 0.3],
+        ["privacy-policy", "never", 0.1],
+        ["counter-strike-global-offensive", "daily", 1],
+        ["counter-strike-global-offensive/my-inventory", "monthly", 0.1],
     ]
 
     @classmethod
     def run(cls, output=None):
-        urls = [url for url in cls.base_urls]
+        urls = [url for url in cls.static_pages]
 
         all_skins = Skin.objects.all()
         already_done = set()
         for skin in all_skins:
             weapon_slug = slugify(skin.weapon.name.value)
-            url = f"https://lionskins.co/counter-strike-global-offensive/{weapon_slug}/{skin.slug}/"
+            url = f"counter-strike-global-offensive/{weapon_slug}/{skin.slug}"
             row = [url, "daily", 0.7]
             if url in already_done:
                 continue
@@ -36,8 +39,16 @@ class GenerateSitemap:
 
         res = """<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">"""
         for url, frequency, priority in urls:
-            res += f"""<url><loc>{url}</loc><changefreq>{frequency}</changefreq><priority>{priority}</priority></url>"""
-        res += """</urlset>"""
+            for language in cls.languages:
+                res += "<url>"
+                res += f"<loc>{cls.base_url}/{language}/{url}</loc>"
+                res += f"<changefreq>{frequency}</changefreq>"
+                res += f"<priority>{priority}</priority>"
+                for other_language in cls.languages:
+                    res += f"""<xhtml:link rel="alternate" hreflang="{other_language}" """
+                    res += f"""href="{cls.base_url}/{language}/{url}" />"""
+                res += "</url>"
+        res += "</urlset>"
 
         if not output:
             sys.stdout.write(res)
