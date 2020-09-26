32ad516ee4f12d418840f028fdd1fb2d297549b1
julien <git@julienc.io>
2020-09-26 18:40:42 +0200

    adding alternative hreflang head

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { ApolloProvider } from '@apollo/client'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { I18nContext } from 'next-i18next'
 import PropTypes from 'prop-types'
 import { client } from '../apollo'
 import { appWithTranslation } from '../i18n'
@@ -15,10 +18,22 @@ import 'slick-carousel/slick/slick.css'
 import '../assets/css/index.scss'
 
 const MyApp = ({ Component, pageProps }) => {
+  const { i18n } = useContext(I18nContext)
+  const router = useRouter()
+  const languages = [i18n.options.defaultLanguage, ...i18n.options.otherLanguages]
+  const path = router.asPath.split('?')[0].slice(4)
   return (
     <ApolloProvider client={client}>
       <AuthenticationProvider>
         <SettingsProvider>
+          <Head>
+            {languages.map(language => (
+              <link
+                key={`alternate-${language}`} rel='alternate' hrefLang={language}
+                href={`${process.env.NEXT_PUBLIC_FRONTEND_DOMAIN}/${language}/${path}`}
+              />
+            ))}
+          </Head>
           <Header />
           <main>
             <Component {...pageProps} />
