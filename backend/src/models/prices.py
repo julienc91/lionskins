0558b1cad9fef56d0f94d8e10942ca2fba94849f
julien <git@julienc.io>
2021-02-13 21:13:55 +0100

    migrating prices.provider to enumfield

--- backend/src/models/prices.py
+++ backend/src/models/prices.py
@@ -9,17 +9,9 @@ from .model_mixin import ModelMixin
 
 class Price(ModelMixin, db.EmbeddedDocument):
 
-    _provider = db.StringField(db_field="provider", choices=Providers, required=True)
+    provider = db.EnumField(Providers, required=True)
     price = db.FloatField(required=True)
     creation_date = db.DateTimeField(required=True, default=datetime.now)
     update_date = db.DateTimeField(requured=True, default=datetime.now)
 
     meta = {"indexes": ["price"]}
-
-    @property
-    def provider(self):
-        return Providers[self._provider]
-
-    @provider.setter
-    def provider(self, value):
-        self._provider = value.name
