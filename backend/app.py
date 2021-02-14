9eeac3e2ea50db8d40ebdeac19d3c71cbe96d1dc
julien <git@julienc.io>
2021-02-14 20:15:56 +0100

    update isort pre-commit config for upcoming import refactoring

--- backend/app.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from src import app
-
-if __name__ == "__main__":
-    app.run()
--- backend/startup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-flask db upgrade
-gunicorn --config gunicorn.py src.app:app
