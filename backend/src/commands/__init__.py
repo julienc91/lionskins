e246c98b237bce8ba58b1d9a790ebd393fe001ee
julien <git@julienc.io>
2021-09-30 19:07:25 +0200

    slight refactoring of fetch_providers

--- backend/src/commands/__init__.py
+++ backend/src/commands/__init__.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import time
-from queue import Queue
-from threading import Thread
 from typing import Optional
 
 import click
@@ -12,13 +9,11 @@ from apscheduler.triggers.cron import CronTrigger
 
 from application import app
 from commands.fetch_players import FetchPlayers
-from commands.fetch_providers import FetchProviders, TaskTypes
+from commands.fetch_providers import FetchProviders
 from commands.generate_sitemap import GenerateSitemap
 from commands.sanity_check import SanityCheck
 from commands.sync_catalog import SyncCatalog
-from models import Skin
 from models.enums import Providers
-from providers.parsers import get_parser
 
 logger = structlog.get_logger()
 
@@ -30,7 +25,7 @@ def backoffice():
     scheduler.add_job(FetchPlayers.run, CronTrigger.from_crontab("0 20 * * *"), id="players")
 
     scheduler.start()
-    _fetch_providers(True, None)
+    FetchProviders(True).run()
 
 
 @app.cli.command("fetch_players")
@@ -41,67 +36,8 @@ def fetch_players():
 @app.cli.command("fetch_providers")
 @click.option("--daemon", is_flag=True)
 @click.option("--provider", type=click.Choice([p.name for p in Providers]))
-def fetch_providers(daemon: bool, provider: Providers):
-    return _fetch_providers(daemon, provider)
-
-
-def _fetch_providers(daemon: bool, provider: Optional[Providers] = None):
-    providers = [Providers[provider]] if provider else list(Providers)
-
-    queue = Queue()
-    remaining_workers = 0
-
-    def worker(p):
-        while True:
-            try:
-                FetchProviders(p, queue).run()
-            except Exception as e:
-                logger.exception(e)
-            queue.put((TaskTypes.LAST_TASK, p))
-            if not daemon:
-                return
-            time.sleep(3600)
-
-    for provider in providers:
-        remaining_workers += 1
-        thread = Thread(target=worker, args=(provider,))
-        thread.start()
-
-    # process all the price updates in the main thread, to avoid concurrency issues
-    while daemon or remaining_workers > 0:
-        task_type, args = queue.get()
-        if task_type == TaskTypes.ADD_PRICE:
-            app_, provider, item_name, price, kwargs = args
-            if price > 0:
-                skin = get_parser(app_).get_or_create_skin_from_item_name(item_name)
-                if skin:
-                    skin.add_price(provider=provider, price=price)
-                    kwargs = kwargs or {}
-                    kwargs = {key: value for key, value in kwargs.items() if getattr(skin, key) != value}
-                    if kwargs:
-                        for key, value in kwargs.items():
-                            setattr(skin, key, value)
-                        skin.save()
-
-        elif task_type == TaskTypes.REMOVE_PRICES:
-            app_, provider, start_date = args
-            for skin in Skin.objects(app=app_):
-                updated_prices = []
-                for price in skin.prices:
-                    if price.provider.name == provider.name and price.update_date < start_date:
-                        continue
-                    updated_prices.append(price)
-
-                if len(updated_prices) != len(skin.prices):
-                    skin.prices = updated_prices
-                    skin.save()
-
-        elif task_type == TaskTypes.LAST_TASK:
-            provider = args
-            remaining_workers -= 1
-            logger.info("Last task received", provider=provider)
-
-        queue.task_done()
+def fetch_providers(daemon: bool, provider: Optional[Providers]):
+    return FetchProviders(daemon, provider).run()
 
 
 @app.cli.command("generate_sitemap")
