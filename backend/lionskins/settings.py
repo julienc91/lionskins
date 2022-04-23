25169c32fb5be572610d1f2a117a7d8ade9a77ca
julien <git@julienc.io>
2022-04-23 15:13:56 +0200

    test: fix CI

--- backend/lionskins/settings.py
+++ backend/lionskins/settings.py
@@ -139,10 +139,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_URL = "static/"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
