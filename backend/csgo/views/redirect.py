6ed3d82cc1f3418f8f661a0c08089d8ccf2d7ec0
julien <git@julienc.io>
2022-04-24 09:20:53 +0200

    fix: fix validation error on legacy redirect url

--- backend/csgo/views/redirect.py
+++ backend/csgo/views/redirect.py
@@ -1,6 +1,7 @@
 import user_agents
+from django.core.exceptions import ValidationError
 from django.http.response import HttpResponseRedirect
-from graphql_relay.node.node import from_global_id
+from graphql_relay import from_global_id
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -20,18 +21,13 @@ def redirect_view(request, provider, skin_id):
 
     try:
         skin = Skin.objects.get(id=skin_id)
-    except Skin.DoesNotExist:
+    except (Skin.DoesNotExist, ValidationError):
         try:
             skin = Skin.objects.get(object_id=skin_id)
         except Skin.DoesNotExist:
             return Response(None, status=status.HTTP_404_NOT_FOUND)
 
-    user_agent = request.headers.get("User-Agent")
-    if (
-        user_agent
-        and not user_agents.parse(user_agent).is_bot
-        and not request.user.is_staff
-    ):
+    if should_create_redirect_object(request):
         if not request.session.session_key:
             request.session.save()
         Redirect.objects.create(
@@ -40,3 +36,12 @@ def redirect_view(request, provider, skin_id):
 
     url = skin.get_skin_url(provider)
     return HttpResponseRedirect(url, status=status.HTTP_302_FOUND)
+
+
+def should_create_redirect_object(request):
+    user_agent = request.headers.get("User-Agent")
+    return (
+        user_agent
+        and not user_agents.parse(user_agent).is_bot
+        and not request.user.is_staff
+    )
